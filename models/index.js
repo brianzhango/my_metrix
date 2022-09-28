f8651c04daa7bc09c5d19cf3244e25c4b2f3d6a2
brianzhango <brianozhang@gmail.com>
2022-09-28 14:14:25 +1000

    Pass Params + Shipment Display

--- models/index.js
+++ models/index.js
@@ -6,6 +6,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.projects = require("./project.model.js")(mongoose);
+// db.projects = require("./project.model.js")(mongoose);
 
 module.exports = db;
