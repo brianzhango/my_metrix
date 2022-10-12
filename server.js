4d02a0ed168da4dea9f954c5da9a26badfb6bfa2
brianzhango <brianozhang@gmail.com>
2022-10-12 12:31:41 +1100

    Test

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ app.use(bodyParser.urlencoded({
 }));
 
 // ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "front-end", "public")))
+app.use(express.static(path.join(__dirname, "../public")))
 
 // ...
 
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "front-end", "public", "index.html"));
+    res.sendFile(path.join(__dirname,  "../public", "index.html"));
 });
 
 const PORT = process.env.PORT || 8082;
