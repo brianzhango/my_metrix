635c81971d874f5547e7a00aefbf92b99906adbe
brianzhango <brianozhang@gmail.com>
2022-10-13 00:16:56 +1100

    Test

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -7,9 +7,18 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [HtmlWebpackPlugin ({
-    favicon: "./public/favicon.ico",
-    filename: "index.html",
-    manifest: "./public/manifest.json"
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, "public", "index.html"),
+  favicon: "./public/favicon.ico",
+  filename: "index.html",
+  manifest: "./public/manifest.json",
 })],
+test: /\.svg$/,
+use: [
+  {
+    loader: 'svg-url-loader',
+    options: {
+      limit: 10000,
+    },
+  },
+],
 };
