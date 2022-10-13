94a432d11ea60f49a182bca8695b4499793f4b0f
brianzhango <brianozhang@gmail.com>
2022-10-14 10:30:35 +1100

    Test

--- server.js
+++ server.js
@@ -49,14 +49,14 @@ db.mongoose
   });
 
 // ... other app.use middleware 
-// app.use(express.static(path.join(__dirname, "/front-end/public")))
-app.use(express.static(path.join(__dirname,"/front-end/public")))
+app.use(express.static(path.join(__dirname, "/front-end/public")))
+
+// ...
 
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname,  "/front-end/public", "index.html"));
 });
-
 
 const PORT = process.env.PORT || 8082;
 
