eef619c18617eee9d10eae62aeababd0ab711d6d
brianzhango <brianozhang@gmail.com>
2022-10-14 08:11:57 +1100

    test

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ var corsOptions = {
   origin: "http://localhost:8081",
 };
 
+// parse requests of content-type - application/json
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
 // app.use(cors(corsOptions));
 app.use(cors());
 
@@ -40,13 +47,6 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
-
-
-// parse requests of content-type - application/json
-app.use(express.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
 // ... other app.use middleware 
 app.use(express.static(path.join(__dirname, "/front-end/public")))
