445691b750f1ae728f210f674237912266b9ee3a
brianzhango <brianozhang@gmail.com>
2022-10-14 07:46:31 +1100

    Test

--- server.js
+++ server.js
@@ -16,28 +16,6 @@ var corsOptions = {
 // app.use(cors(corsOptions));
 app.use(cors());
 
-// parse requests of content-type - application/json
-app.use(express.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "/front-end/public")))
-
-// ...
-
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname,  "/front-end/public", "index.html"));
-});
-
-const PORT = process.env.PORT || 8082;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-
 app.use("/api/jobs", require("./routes/job.route"));
 
 app.use("/api/users", require("./routes/user.route"));
@@ -62,3 +40,27 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+
+
+// parse requests of content-type - application/json
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "/front-end/public")))
+
+// ...
+
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,  "/front-end/public", "index.html"));
+});
+
+const PORT = process.env.PORT || 8082;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
